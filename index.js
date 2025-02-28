b4c98b409cee371d50c877e1b2e33a2d3de84347
timothychemweno254 <timothy.kiprop@student.moringaschool.com>
2025-02-28 19:06:45 +0300

    object js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--; // Decrease the number by 1 each time
+    }
+  }
+  
+  function writeCards(names) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+  }
+  
